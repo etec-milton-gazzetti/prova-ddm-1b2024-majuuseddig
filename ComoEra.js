2c9d763133230bd73036a625fe310cc3ff7f9983
majuu <141245082+majuuseddig@users.noreply.github.com>
2024-04-13 01:54:43 +0000

    PROVA

--- ComoEra.js
+++ ComoEra.js
@@ -1,20 +1,12 @@
 import React from "react";
-import {
-  ScrollView,
-  View,
-  Text,
-  Image,
-  Button,
-  StyleSheet,
-} from "react-native";
+import { ScrollView, View, Text, Button, StyleSheet } from "react-native";
 
 const ComoEra = ({ navigation }) => {
   return (
     <ScrollView contentContainerStyle={styles.container}>
-       <Image source={require("./assets/comoera.png")} style={styles.backgroundImage} />
       <View style={styles.content}>
-        <Text style={styles.title}>Como era o Egito Antigo</Text>
-        <Text>
+        <Text style={[styles.title, styles.firstTitle]}>Como era o Egito Antigo</Text>
+        <Text style={styles.text}>
           O Egito Antigo foi uma civilização milenar que floresceu ao longo do
           rio Nilo, no nordeste da África. Dependendo das cheias anuais do rio,
           os egípcios desenvolveram uma sociedade hierárquica, onde o faraó
@@ -27,15 +19,15 @@ const ComoEra = ({ navigation }) => {
           histórica.
         </Text>
         <Text style={styles.title}>Pirâmide social</Text>
-        <Text>
+        <Text style={styles.text}>
           A sociedade do Antigo Egito era hierárquica, organizada em uma
           pirâmide social. Na base estavam os camponeses e trabalhadores. Acima
           estavam os artesãos, comerciantes e profissionais. Mais acima estavam
           os funcionários públicos e sacerdotes. No topo estava o faraó,
-          considerado divino e com poder absoluto
+          considerado divino e com poder absoluto.
         </Text>
         <Text style={styles.title}>Civilização egípcia</Text>
-        <Text>
+        <Text style={styles.text}>
           A civilização egípcia floresceu ao longo do rio Nilo por milênios,
           deixando um legado impressionante em arquitetura, arte e ciência.
           Governada por faraós, sua sociedade era hierárquica e profundamente
@@ -44,7 +36,7 @@ const ComoEra = ({ navigation }) => {
           influenciar a história e a cultura até os dias de hoje.
         </Text>
         <Text style={styles.title}>Economia egípcia</Text>
-        <Text>
+        <Text style={styles.text}>
           A economia do Antigo Egito era agrícola e baseada nas cheias do rio
           Nilo. Culturas como trigo, cevada e linho eram cultivadas. O comércio
           de produtos como papiro e ouro também era importante. O Estado
@@ -63,22 +55,29 @@ const ComoEra = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   container: {
+    flexGrow: 1,
     alignItems: "center",
+    backgroundColor: "#E5C98B",
     paddingVertical: 20,
-  },
-  backgroundImage: {
-    width: "100%",
-    height: 200,
-    resizeMode: "cover",
   },
   content: {
     paddingHorizontal: 20,
-    paddingTop: 20,
+    paddingTop: 10,
   },
   title: {
     fontSize: 20,
     fontWeight: "bold",
     marginBottom: 10,
+    color: "#fff",
+    textAlign: "center",
+  },
+  firstTitle: {
+    marginTop: 20,
+  },
+  text: {
+    fontSize: 16,
+    marginBottom: 20,
+    color: "#fff",
   },
 });
 
--- MitologiaEgipicia.js
+++ MitologiaEgipicia.js
@@ -1,59 +1,47 @@
 import React from "react";
-import {
-  ScrollView,
-  View,
-  Text,
-  Image,
-  Button,
-  StyleSheet,
-} from "react-native";
+import { ScrollView, View, Text, Button, StyleSheet } from "react-native";
 
-const MitologiaEgipcia = ({ navigation }) => {
+const ComoEra = ({ navigation }) => {
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <Image
-        source={require("./assets/mitologia.png")}
-        style={styles.backgroundImage}
-      />
       <View style={styles.content}>
-        <Text style={styles.title}>O que é Mitologia Egípcia</Text>
-        <Text>
-          A mitologia egípcia é o conjunto de crenças, histórias e rituais
-          religiosos praticados pelos antigos egípcios. Ela envolve uma ampla
-          variedade de deuses e deusas que representavam diferentes aspectos da
-          natureza, da vida humana e do cosmos. A mitologia egípcia inclui
-          narrativas sobre a criação do mundo, a vida após a morte, o papel dos
-          faraós como intermediários entre os deuses e os humanos, e rituais de
-          adoração e culto. Essas histórias e crenças eram fundamentais para a
-          sociedade egípcia, influenciando sua cultura, arte, arquitetura e
-          práticas religiosas.
+        <Text style={[styles.title, styles.firstTitle]}>Como era o Egito Antigo</Text>
+        <Text style={[styles.text, styles.firstText]}>
+          O Egito Antigo foi uma civilização milenar que floresceu ao longo do
+          rio Nilo, no nordeste da África. Dependendo das cheias anuais do rio,
+          os egípcios desenvolveram uma sociedade hierárquica, onde o faraó
+          reinava como um governante divino. Sua religião, rica em deuses e
+          rituais, permeava todas as áreas da vida. Eles deixaram um legado
+          impressionante de arte, arquitetura e avanços científicos, como a
+          escrita hieroglífica e técnicas de construção de monumentos
+          duradouros, incluindo as majestosas pirâmides. O Egito Antigo continua
+          a intrigar e fascinar o mundo moderno com sua riqueza cultural e
+          histórica.
         </Text>
-        <Text style={styles.title}>Fontes de estudo da mitologia egípcia</Text>
-        <Text>
-          O estudo sobre a mitologia egípcia é realizado de várias formas, como
-          através de pirâmides, túmulos, estátuas e fontes escritas. As
-          principais fontes escritas utilizadas são o Livro das Pirâmides, o
-          Livro dos Sarcófagos, o Livro dos Mortos, os textos de Heródoto e os
-          textos de Plutarco.
+        <Text style={styles.title}>Pirâmide social</Text>
+        <Text style={styles.text}>
+          A sociedade do Antigo Egito era hierárquica, organizada em uma
+          pirâmide social. Na base estavam os camponeses e trabalhadores. Acima
+          estavam os artesãos, comerciantes e profissionais. Mais acima estavam
+          os funcionários públicos e sacerdotes. No topo estava o faraó,
+          considerado divino e com poder absoluto.
         </Text>
-        <Text style={styles.title}>Principais deuses egípcios</Text>
-        <Text>
-        Rá-Atum: Rá é o principal deus egípcio e representa o Sol.
-        Shu: Shu é o deus do ar. 
-        Tefnut: Tefnut é a deusa da umidade. 
-        Geb: Geb é o deus da terra. 
-        Nut: Nut é a deusa dos céus. 
-        Set: Set é o deus do caos, ele representa a guerra e a escuridão. 
-        Osíris: Osíris se tornou o deus supremo e o juiz do mundo dos mortos. 
-        Ísis: Ísis é a deusa da magia e do amor.
-        Nephthys: Nephthys é a deusa associada ao culto dos mortos.
-        Hórus: Hórus é o deus protetor dos faraós e das famílias.
-        Hathor: Hathor é a deusa guardiã das mulheres, sobretudo as grávidas, e protetora dos amantes. 
-        Anúbis: Anúbis é o deus responsável pela passagem para o mundo dos mortos. 
-        Maat: Maat é a deusa da verdade e da justiça.
-        Sekhmeth: Sekhmeth é a deusa associada ao aspecto destrutivo do Sol. 
-        Bastet: Bastet é a deusa da sexualidade e do parto e é atrelada à fertilidade. 
-        Thoth: Thoth é o deus da Lua, da sabedoria e da cura. 
+        <Text style={styles.title}>Civilização egípcia</Text>
+        <Text style={styles.text}>
+          A civilização egípcia floresceu ao longo do rio Nilo por milênios,
+          deixando um legado impressionante em arquitetura, arte e ciência.
+          Governada por faraós, sua sociedade era hierárquica e profundamente
+          religiosa. Eles desenvolveram a escrita hieroglífica e construíram
+          monumentos icônicos como as pirâmides. Seu legado continua a
+          influenciar a história e a cultura até os dias de hoje.
+        </Text>
+        <Text style={styles.title}>Economia egípcia</Text>
+        <Text style={styles.text}>
+          A economia do Antigo Egito era agrícola e baseada nas cheias do rio
+          Nilo. Culturas como trigo, cevada e linho eram cultivadas. O comércio
+          de produtos como papiro e ouro também era importante. O Estado
+          controlava a terra e cobrava impostos, e havia uma classe de artesãos
+          e comerciantes.
         </Text>
       </View>
       <Button
@@ -67,23 +55,33 @@ const MitologiaEgipcia = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   container: {
+    flexGrow: 1,
     alignItems: "center",
+    backgroundColor: "#E5C98B",
     paddingVertical: 20,
-  },
-  backgroundImage: {
-    width: "100%",
-    height: 200,
-    resizeMode: "cover",
   },
   content: {
     paddingHorizontal: 20,
-    paddingTop: 20,
+    paddingTop: 10,
   },
   title: {
     fontSize: 20,
     fontWeight: "bold",
     marginBottom: 10,
+    color: "#fff",
+    textAlign: "center",
+  },
+  firstTitle: {
+    marginTop: 20,
+  },
+  text: {
+    fontSize: 20,
+    marginBottom: 20,
+    color: "#fff",
+  },
+  firstText: {
+    marginTop: 5,
   },
 });
 
-export default MitologiaEgipcia;
+export default ComoEra;
